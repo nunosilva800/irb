a2a92c518f41ca88dabbe09358c710231f6412dd
Stan Lo <stan001212@gmail.com>
2023-08-09 13:31:43 +0100

    Refactor ShowCmds

--- lib/irb/cmd/show_cmds.rb
+++ lib/irb/cmd/show_cmds.rb
@@ -13,48 +13,21 @@ module IRB
       description "List all available commands and their description."
 
       def execute(*args)
-        commands_info = IRB::ExtendCommandBundle.all_commands_info
-        commands_grouped_by_categories = commands_info.group_by { |cmd| cmd[:category] }
-
-        if irb_context.with_debugger
-          # Remove the original "Debugging" category
-          commands_grouped_by_categories.delete("Debugging")
-          # Remove the `help` command as it's delegated to the debugger
-          commands_grouped_by_categories["Context"].delete_if { |cmd| cmd[:display_name] == :help }
-        end
-
-        longest_cmd_name_length = commands_info.map { |c| c[:display_name].length }.max
-
         output = StringIO.new
 
-        commands_grouped_by_categories.each do |category, cmds|
-          output.puts Color.colorize(category, [:BOLD])
+        commands_info = IRB::ExtendCommandBundle.all_commands_info
 
-          cmds.each do |cmd|
-            output.puts "  #{cmd[:display_name].to_s.ljust(longest_cmd_name_length)}    #{cmd[:description]}"
+        if irb_context.with_debugger
+          commands_info.delete_if do |cmd|
+            # Remove debugging commands and `help` because they are delegated to the debugger
+            cmd[:category] == "Debugging" || cmd[:display_name] == :help
           end
-
-          output.puts
         end
+
+        generate_section("[Commands]", commands_info, output)
 
         helpers_info = IRB::HelperMethod.all_helper_methods_info
-
-        unless helpers_info.empty?
-          output.puts(Color.colorize("[Helper Methods]", [:BOLD]) + "\n\n")
-        end
-
-        helpers_grouped_by_categories = helpers_info.group_by { |cmd| cmd[:category] }
-        longest_helper_name_length = helpers_info.map { |c| c[:display_name].length }.max
-
-        helpers_grouped_by_categories.each do |category, helpers|
-          output.puts Color.colorize(category, [:BOLD])
-
-          helpers.each do |helper|
-            output.puts "  #{helper[:display_name].to_s.ljust(longest_helper_name_length)}    #{helper[:description]}"
-          end
-
-          output.puts
-        end
+        generate_section("[Helper Methods]", helpers_info, output)
 
         # Append the debugger help at the end
         if irb_context.with_debugger
@@ -63,6 +36,25 @@ module IRB
         end
 
         Pager.page_content(output.string)
+      end
+
+      private
+
+      def generate_section(title, entries, output)
+        output.puts(Color.colorize(title, [:BOLD]) + "\n\n")
+
+        entries_grouped_by_categories = entries.group_by { |cmd| cmd[:category] }
+        longest_name_length = entries.map { |c| c[:display_name].length }.max
+
+        entries_grouped_by_categories.each do |category, entries|
+          output.puts Color.colorize(category, [:BOLD])
+
+          entries.each do |entry|
+            output.puts "  #{entry[:display_name].to_s.ljust(longest_name_length)}    #{entry[:description]}"
+          end
+
+          output.puts
+        end
       end
     end
   end
